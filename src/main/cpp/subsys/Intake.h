2261d4f9021623c8ce6e70268ca0071b6d7ba778
Alyssa Kearns <akearns3195@gmail.com>
2020-01-13 18:21:26 -0500

    intake

--- src/main/cpp/subsys/Intake.h
+++ src/main/cpp/subsys/Intake.h
@@ -25,3 +25,78 @@
 #include <subsys/IMechanism.h>
 // Third Party Includes
 
+
+/*================================================================================================
+ * Intake.h
+ *================================================================================================
+ * 
+ * File Description: This class controls the intake wheels
+ * 
+ *================================================================================================*/
+class Intake
+{
+    public:
+
+        /// @brief          Indicates the type of mechanism this is
+        /// @return         MechanismTypes::MECHANISM_TYPE
+        virtual MechanismTypes::MECHANISM_TYPE GetType() const = 0;
+
+
+        /// @brief      Run mechanism as defined 
+        /// @param [in] ControlModes::CONTROL_TYPE   controlType:  How are the item(s) being controlled
+        /// @param [in] double                                     value:        Target (units are based on the controlType)
+        /// @return     void
+        virtual void SetOutput
+        (
+            ControlModes::CONTROL_TYPE controlType,
+            double                                   value       
+        ) override;
+
+        /// @brief      Activate/deactivate pneumatic solenoid
+        /// @param [in] bool - true == extend, false == retract
+        /// @return     void 
+        virtual void ActivateSolenoid
+        (
+            bool     activate
+        ) override;
+
+        /// @brief      Check if the pneumatic solenoid is activated
+        /// @return     bool - true == extended, false == retract
+        virtual bool IsSolenoidActivated() = 0;
+
+
+        /// @brief  Return the current position of the mechanism.  The value is in inches or degrees.
+        /// @return double	position in inches (translating mechanisms) or degrees (rotating mechanisms)
+        virtual double GetCurrentPosition() const = 0;
+
+        /// @brief  Return the targget position of the mechanism.  The value is in inches or degrees.
+        /// @return double	position in inches (translating mechanisms) or degrees (rotating mechanisms)
+        virtual double GetTargetPosition() const = 0;
+
+        /// @brief  Get the current speed of the mechanism.  The value is in inches per second or degrees per second.
+        /// @return double	speed in inches/second (translating mechanisms) or degrees/second (rotating mechanisms)
+        virtual double GetCurrentSpeed() const =0;
+
+
+        /// @brief  Get the target speed of the mechanism.  The value is in inches per second or degrees per second.
+        /// @param [in] ControlModes::MECHANISM_CONTROL_ID     controlItems: What item(s) are being requested
+        /// @return double	speed in inches/second (translating mechanisms) or degrees/second (rotating mechanisms)
+        virtual double GetTargetSpeed() const = 0;
+
+
+        /// @brief  Set the control constants (e.g. PIDF values).
+        /// @param [in] ControlData*                                   pid:  the control constants
+        /// @return void
+        virtual void SetControlConstants
+        (
+            ControlData*                                pid                 
+        ) override;
+
+
+	IMechanism() = default;
+	virtual ~IMechanism() = default;
+};
+typedef std::vector<IMechanism*> IMechanismVector;
+
+    private:
+}
