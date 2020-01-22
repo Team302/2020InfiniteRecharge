309413bb878f5b8f5b405bbbf60537ca64293a2d
Neethan Punniyamoorthy <LOPunniyamoorthyN07@stu.lok12.org>
2020-01-21 20:39:54 -0500

    TurretAim state

---
+++ src/main/cpp/controllers/turret/ManualAim.cpp
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "controllers/turret/ManualAim.h"
+#include "subsys/MechanismFactory.h"
+#include "subsys/IMechanism.h"
+#include "subsys/MechanismParameters.h"
+#include "subsys/Turret.h"
+#include "controllers/ControlData.h"
+ManualAim::ManualAim(ControlData* controlData, double mechData) : m_controlData(controlData),
+    m_mechData(mechData), 
+    m_atTarget(false)
+{
+    auto factory = MechanismFactory::GetMechanismFactory();
+    m_turret = factory->GetIMechanism(MechanismTypes::TURRET);
+}
+
+void ManualAim::Init()
+{
+
+}
+
+void ManualAim::Run()
+{
+    m_turret->SetOutput(ControlModes::PERCENT_OUTPUT, m_mechData);
+}
+
+bool ManualAim::AtTarget() const
+{
+    return m_atTarget;
+}
---
+++ src/main/cpp/controllers/turret/ManualAim.h
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <controllers/IState.h>
+#include <controllers/mechanisms/IMechController.h>
+#include <controllers/ControlData.h>  
+#include <subsys/Turret.h>
+
+class ManualAim : public IState
+{
+ public:
+  ManualAim
+  (
+    ControlData* controlData,
+    double mechData //to be changed to MechData type
+  );
+  void Init() override;
+  void Run() override;
+  bool AtTarget() const override;
+
+ private:
+  
+  ControlData* m_controlData;
+  double m_mechData;
+  bool m_atTarget;
+  IMechanism* m_turret;
+
+};
