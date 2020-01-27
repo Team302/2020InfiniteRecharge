ccfe9d023613a8f41b946aaf2371f11d464fa539
Neethan Punniyamoorthy <LOPunniyamoorthyN07@stu.lok12.org>
2020-01-27 18:34:15 -0500

    Position control for manual turning

--- src/main/cpp/controllers/turret/ManualAim.cpp
+++ src/main/cpp/controllers/turret/ManualAim.cpp
@@ -26,7 +26,7 @@ void ManualAim::Init()
 
 void ManualAim::Run()
 {
-    m_turret->SetOutput(m_controlData->GetMode(), TeleopControl::GetInstance()->GetAxisValue(TeleopControl::SHOOTER_MANUAL_AIM));
+    m_turret->SetOutput(m_controlData->GetMode(), TeleopControl::GetInstance()->GetAxisValue(TeleopControl::SHOOTER_MANUAL_AIM) * 45.0); //scaled to a percentage of a 45 degree turn
     m_atTarget = true;
 }
 
