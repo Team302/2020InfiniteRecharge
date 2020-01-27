2396beb7ac6326de860a4e7267ed5a56f7e9f971
2Percent-Milk <beanravine@gmail.com>
2020-01-27 18:59:30 -0500

    Documentation
    Documentation added to cpp

--- src/main/cpp/controllers/controlpanel/ControlPanelTurn.cpp
+++ src/main/cpp/controllers/controlpanel/ControlPanelTurn.cpp
@@ -13,17 +13,22 @@ ControlPanelTurn::ControlPanelTurn(ControlData* control_data, double target)
     m_control_data = control_data;
 }
 
+// Creates the desired target location for the spin to end at
+
 void ControlPanelTurn::Init()
 {
     m_target = m_control_panel_manipulator -> GetCurrentPosition() + m_offset;
     m_control_panel_manipulator -> SetControlConstants(m_control_data);
 }
 
+// Spins the wheel to the target
+
 void ControlPanelTurn::Run()
 {
     m_control_panel_manipulator -> SetOutput(m_control_data -> GetMode(), m_target);
-    m_current_position = m_control_panel_manipulator -> GetCurrentPosition();
 }
+
+// Performs a check to see if the panel has been spun to the target point yet
 
 bool ControlPanelTurn::AtTarget() const
 {
