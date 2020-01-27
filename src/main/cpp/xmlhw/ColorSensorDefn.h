29c29e0ec6f851598cad5a481f2d9fb94e27f8e6
Hong Bing Tang <htbang04@gmail.com>
2020-01-26 21:30:27 -0500

    ColorSensorV3

---
+++ src/main/cpp/xmlhw/ColorSensorDefn.h
@@ -0,0 +1,21 @@
+
+
+#include <pugixml/pugixml.hpp>
+
+#include <ctre/Phoenix.h>
+#include <rev/ColorSensorV3.h>
+using namespace std;
+
+
+class ColorSensorDefn
+{
+    public:
+    ColorSensorDefn() = default;
+    virtual ~ColorSensorDefn() = default;
+    ColorSensorDefn* ParseXML
+    (
+    
+    pugi::xml_node  ColorSensorNode
+
+    );
+};
