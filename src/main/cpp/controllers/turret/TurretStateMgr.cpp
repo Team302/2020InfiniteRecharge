56a6cd7f58a2c9870f8074c2dd8f25fa3bb9a143
Neethan Punniyamoorthy <LOPunniyamoorthyN07@stu.lok12.org>
2020-02-11 18:50:14 -0500

    last night stuff

--- src/main/cpp/controllers/turret/TurretStateMgr.cpp
+++ src/main/cpp/controllers/turret/TurretStateMgr.cpp
@@ -6,5 +6,114 @@
 /*----------------------------------------------------------------------------*/
 
 #include "controllers/turret/TurretStateMgr.h"
+#include <controllers/IState.h>
+#include <controllers/shooterHood/ShooterHoodStateMgr.h>
+#include <xmlmechdata/StateDataDefn.h>
+#include <controllers/MechanismTargetData.h>
+#include <utils/Logger.h>
+#include <gamepad/TeleopControl.h>
 
-TurretStateMgr::TurretStateMgr() {}
+#include <controllers/turret/ManualAim.h>
+#include <controllers/turret/LimelightAim.h>
+#include <controllers/turret/HoldTurretPosition.h>
+#include <hw/factories/LimelightFactory.h>
+
+using namespace std;
+
+TurretStateMgr::TurretStateMgr() : m_stateMap(),
+                            m_currentState()
+{
+    // Parse the configuration file 
+    auto stateXML = make_unique<StateDataDefn>();
+    vector<MechanismTargetData*> targetData = stateXML.get()->ParseXML( MechanismTypes::MECHANISM_TYPE::TURRET );
+
+    map<string, TURRET_STATE> stateMap;
+    stateMap["TURRETHOLD"] = TURRET_STATE::HOLD;
+    stateMap["TURRETAUTOAIM"] = TURRET_STATE::LIMELIGHT_AIM;
+    stateMap["TURRETMANUALAIM"] = TURRET_STATE::MANUAL_AIM;
+
+    for ( auto td: targetData )
+    {
+        auto stateString = td->GetStateString();
+        auto stateItr = stateMap.find( stateString );
+        if ( stateItr != stateMap.end() )
+        {
+            auto stateEnum = stateItr->second;
+            auto stateIt = m_stateMap.find( stateEnum );
+            if ( stateIt == m_stateMap.end() )
+            {
+                auto controlData = td->GetController();
+                auto target = td->GetTarget();
+
+                switch ( stateEnum )
+                {
+                    case TURRET_STATE::HOLD:
+                    {
+                        auto thisState = new HoldTurretPosition(controlData, target, MechanismTargetData::SOLENOID::NONE);
+                        m_stateMap[TURRET_STATE::HOLD] = thisState;
+                        m_currentState = thisState;
+                        m_currentStateEnum = stateEnum;
+                        m_currentState->Init();
+                    }
+                    break;
+
+                    case TURRET_STATE::LIMELIGHT_AIM:
+                    {
+                        auto thisState = new LimelightAim(controlData);
+                        m_stateMap[TURRET_STATE::LIMELIGHT_AIM] = thisState;
+                    }
+                    break;
+
+                    case TURRET_STATE::MANUAL_AIM:
+                    {
+                        auto thisState = new ManualAim(controlData);
+                        m_stateMap[TURRET_STATE::MANUAL_AIM] = thisState;
+                    }
+                    break;
+
+                    default:
+                    {
+                        Logger::GetLogger()->LogError( string("TurretHoodStateMgr::TurretHoodStateMgr"), string("unknown state"));
+                    }
+                    break;
+                }
+            }
+            {
+                
+            }
+            
+        }
+    }
+}
+
+void TurretStateMgr::RunCurrentState()
+{
+    // process teleop/manual interrupts
+    auto controller = TeleopControl::GetInstance();
+    if ( controller != nullptr )
+    {
+        
+    }
+}
+void TurretStateMgr::SetCurrentState
+(
+    TURRET_STATE stateEnum,
+    bool         run
+)
+{
+    auto itr = m_stateMap.find( stateEnum );
+    if ( itr != m_stateMap.end() )
+    {
+        auto state = itr->second;
+        if ( state != m_currentState )
+        {
+            m_currentState = state;
+            m_currentStateEnum = stateEnum;
+            m_currentState->Init();
+            if ( run )
+            {
+                m_currentState->Run();
+            }
+        }
+}
+}
