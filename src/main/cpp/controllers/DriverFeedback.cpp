f18182a86e390561538b9232a3ab5dbfac439947
AJ Gdowski <LOGdowskiA56@stu.lok12.org>
2020-02-22 10:58:28 -0500

    Made driver feedback system

--- src/main/cpp/controllers/DriverFeedback.cpp
+++ src/main/cpp/controllers/DriverFeedback.cpp
@@ -13,24 +13,54 @@
 // OR OTHER DEALINGS IN THE SOFTWARE.
 //====================================================================================================================================================
 
+#include <string>
+
 #include <controllers/DriverFeedback.h>
 #include <states/balltransfer/BallTransferStateMgr.h>
-#include <states/chassis/ChassisStateMgr.h>
+//#include <states/chassis/ChassisStateMgr.h>
 #include <states/controlPanel/ControlPanelStateMgr.h>
 #include <states/impeller/ImpellerStateMgr.h>
 #include <states/intake/IntakeStateMgr.h>
 #include <states/shooter/ShooterStateMgr.h>
 #include <states/shooterHood/ShooterHoodStateMgr.h>
 #include <states/turret/TurretStateMgr.h>
-#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/SmartDashboard/SmartDashboard.h>
 
+using namespace frc;
+using namespace std;
 
 DriverFeedback::DriverFeedback()
 {
-    
+    m_ballTransfer = BallTransferStateMgr::GetInstance();
+    //m_chassis = ChassisStateMgr::GetInstance();
+    m_controlPanel = ControlPanelStateMgr::GetInstance();
+    m_impeller = ImpellerStateMgr::GetInstance();
+    m_intake = IntakeStateMgr::GetInstance();
+    m_shooter = ShooterStateMgr::GetInstance();
+    m_shooterHood = ShooterHoodStateMgr::GetInstance();
+    m_turret = TurretStateMgr::GetInstance();
 }
 
 void DriverFeedback::periodic()
 {
-    
+    auto ballTransferState = m_ballTransfer -> GetCurrentState();
+    SmartDashboard::PutString( string("Ball Transfer State") ,to_string(ballTransferState));
+
+    auto controlPanelState = m_controlPanel -> GetCurrentState();
+    SmartDashboard::PutString( string("Control Panel State"), to_string(controlPanelState));
+
+    auto impellerState = m_impeller -> GetCurrentState();
+    SmartDashboard::PutString( string("Impeller State"), to_string(impellerState));
+
+    auto intakeState = m_intake -> GetCurrentState();
+    SmartDashboard::PutString( string("Intake State"), to_string(intakeState));
+
+    auto shooterState = m_shooter -> GetCurrentState();
+    SmartDashboard::PutString( string("Shooter State"), to_string(shooterState));
+
+    auto shooterHoodState = m_shooterHood -> GetCurrentState();
+    SmartDashboard::PutString( string("Shooter Hood State"), to_string(shooterHoodState));
+
+    auto turretState = m_turret -> GetCurrentState();
+    SmartDashboard::PutString( string("Turret State"), to_string(turretState));
 }
--- src/main/cpp/controllers/DriverFeedback.h
+++ src/main/cpp/controllers/DriverFeedback.h
@@ -1,7 +1,7 @@
 #pragma once 
 
 #include <states/balltransfer/BallTransferStateMgr.h>
-#include <states/chassis/ChassisStateMgr.h>
+//#include <states/chassis/ChassisStateMgr.h>
 #include <states/controlPanel/ControlPanelStateMgr.h>
 #include <states/impeller/ImpellerStateMgr.h>
 #include <states/intake/IntakeStateMgr.h>
@@ -16,5 +16,15 @@ class DriverFeedback
     void periodic();
     DriverFeedback();
     ~DriverFeedback() = delete;
+
+    private:
+    BallTransferStateMgr* m_ballTransfer;
+    //ChassisStateMgr* m_chassis;
+    ControlPanelStateMgr* m_controlPanel;
+    ImpellerStateMgr* m_impeller;
+    IntakeStateMgr* m_intake;
+    ShooterStateMgr* m_shooter;
+    ShooterHoodStateMgr* m_shooterHood;
+    TurretStateMgr* m_turret;
    
 };
