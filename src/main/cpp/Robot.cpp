8b4826e18e499592ab946e6e6a1ff9cfe54f768a
Joe Witcpalek <joe.witcpalek@gmail.com>
2020-02-26 19:47:24 -0500

    push garbage code up

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,310 +1,221 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
-//====================================================================================================================================================
-// Copyright 2020 Lake Orion Robotics FIRST Team 302
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-// OR OTHER DEALINGS IN THE SOFTWARE.
-//====================================================================================================================================================
+#include "Robot.h"
 
-// C++ Includes
 #include <iostream>
-#include <memory>
 
-// FRC includes
 #include <frc/smartdashboard/SmartDashboard.h>
-#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/Solenoid.h>
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonFX.h>
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonSRX.h>
+#include <frc/XboxController.h>
+#include <frc/GenericHID.h>
 
-// Team 302 Includes
-#include <Robot.h>
-#include <xmlhw/RobotDefn.h>
-#include <auton/CyclePrimitives.h>
-#include <states/chassis/ChassisStateMgr.h>
-#include <states/BallManipulator.h>
-#include <states/intake/IntakeStateMgr.h>
-#include <gamepad/TeleopControl.h>
-//#include <states/controlPanel/ControlPanelStateMgr.h>
-//#include <states/climber/ClimberStateMgr.h>
-#include <hw/factories/LimelightFactory.h>
-#include <hw/DragonLimelight.h>
-
-#include <ctre/Phoenix.h>
+#include <ctre/phoenix/motorcontrol/SupplyCurrentLimitConfiguration.h>
 
 
-#include <test/ButtonBoxDisplay.h>
-#include <test/XboxDisplay.h>
-#include <test/IntakeStateMgrTest.h>
-#include <test/ImpellerStateMgrTest.h>
-#include <test/BallTransferStateMgrTest.h>
-#include <test/ShooterStateMgrTest.h>
-#include <ctre/Phoenix.h>
-#include <subsys/MechanismFactory.h>
-#include <subsys/MechanismTypes.h>
-#include <subsys/ChassisFactory.h>
-#include <subsys/IChassis.h>
-#include <hw/factories/PigeonFactory.h>
-
-
-
-using namespace std;
 using namespace frc;
+using namespace std;
+using namespace ctre::phoenix;
+using namespace ctre::phoenix::motorcontrol;
+using namespace ctre::phoenix::motorcontrol::can;
 
-/// @brief  The main robot code.  The Init methods get called when that state gets entered and then the 
-///     Periodic methods get called every 20 milliseconds.
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
-/// @brief When the robot gets created this gets called.  It initializes the robot subsystems (hardware).
-/// @return void
-void Robot::RobotInit() 
-{
-    // Read the robot definition from the xml configuration files and
-    // create the hardware (chassis + mechanisms along with their talons,
-    // solenoids, digital inputs, analog inputs, etc.
-    unique_ptr<RobotDefn>  robotXml = make_unique<RobotDefn>();
-    robotXml->ParseXML();
+  auto limit = SupplyCurrentLimitConfiguration( true, 40.0, 50.0, 15.0 );
 
-    // Display the autonomous choices on the dashboard for selection.
-    // comment out for now since auton hasn't been implemented
-    //m_cyclePrims = new CyclePrimitives();
+  m_left1 = new WPI_TalonFX( 12 );
+  m_left1->SetInverted( true );
+  m_left1->ConfigSupplyCurrentLimit( limit, 50 );
 
-    // Create the Chassis Control (state) modes which puts the auton choices and teleop drive modes 
-    // on the dashboard for selection.
-    m_chassisStateMgr = new ChassisStateMgr();
+  m_left2 = new WPI_TalonFX( 13 );
+  m_left2->SetInverted( true );
+  m_left2->ConfigSupplyCurrentLimit( limit, 50 );
 
-    /*m_leftMaster = new TalonFX(12);
-    m_leftSlave = new TalonFX(13);
-    m_rightMaster = new TalonFX(15);
-    m_rightSlave = new TalonFX(14);
+  m_right1 = new WPI_TalonFX( 14 );
+  m_right1->ConfigSupplyCurrentLimit( limit, 50 );
 
-    m_leftMaster->ConfigFactoryDefault();
-    m_leftSlave->ConfigFactoryDefault();
-    m_rightMaster->ConfigFactoryDefault();
-    m_rightSlave->ConfigFactoryDefault();
+  m_right2 = new WPI_TalonFX( 15 );
+  m_right2->ConfigSupplyCurrentLimit( limit, 50  );
 
-    m_leftSlave->SetInverted(false);
-    m_leftMaster->SetInverted(false);
-    m_rightMaster->SetInverted(true);
-    m_rightSlave->SetInverted(true);
+  m_shooter1 = new WPI_TalonFX( 1 );
+  m_shooter1->SetInverted( true );
+  m_shooter1->ConfigSupplyCurrentLimit( limit, 50 );
 
-    m_leftSlave->Set(ControlMode::Follower, 12);
-    m_rightSlave->Set(ControlMode::Follower, 15);*/
+  m_shooter2 = new WPI_TalonFX( 16 );
+  m_shooter2->ConfigSupplyCurrentLimit( limit, 50 );
 
-    //m_intake = new IntakeStateMgr();
-    m_powerCells = BallManipulator::GetInstance();
-    m_shooterHood = MechanismFactory::GetMechanismFactory()->GetIMechanism(MechanismTypes::SHOOTER_HOOD);
-    m_turret = MechanismFactory::GetMechanismFactory()->GetIMechanism(MechanismTypes::TURRET);
-    m_impeller = MechanismFactory::GetMechanismFactory()->GetIMechanism(MechanismTypes::IMPELLER);
-    m_shooter = MechanismFactory::GetMechanismFactory()->GetIMechanism(MechanismTypes::SHOOTER);
-    m_controller = TeleopControl::GetInstance();
-    //m_chassis = ChassisFactory::GetChassisFactory()->GetIChassis(); 
+  m_intake = new WPI_TalonSRX( 11 );
 
-    // m_control = new ControlPanelStateMgr();
-    // m_climber = new ClimberStateMgr();
+  m_impeller = new WPI_TalonSRX( 7 );
+  m_transfer = new WPI_TalonSRX( 3 );
+  m_shooterHood = new WPI_TalonSRX( 4 );
+  m_turret = new WPI_TalonSRX( 5 );
+  m_cpm = new WPI_TalonSRX( 6 );
+  m_winch = new WPI_TalonSRX( 2 );
 
-        // pick test mode
-    m_testChooser.SetDefaultOption( m_noTest, m_noTest);
-    m_testChooser.AddOption( m_buttonBoxTest, m_buttonBoxTest );
-    m_testChooser.AddOption( m_dragonXBoxTest, m_dragonXBoxTest );    
-    m_testChooser.AddOption( m_intakeTest, m_intakeTest );
-    m_testChooser.AddOption( m_impellerTest, m_impellerTest );
-    m_testChooser.AddOption( m_ballTransferTest, m_ballTransferTest );
-//    m_testChooser.AddOption( m_shooterTest, m_shooterTest );
+  
+  m_intakeSol = new Solenoid( 9, 4 );
+  m_cpmSol = new Solenoid( 9, 5 );
+  m_climberSol = new Solenoid( 9, 6 );
 
-    SmartDashboard::PutData("Test", &m_testChooser);
+  m_driver = new XboxController( 0 );
+  m_pilot = new XboxController( 1 );
 
-    m_buttonBoxDisplay = nullptr;
-    m_xBoxDisplay = nullptr;
 
-    //m_limelight = LimelightFactory::GetLimelightFactory()->GetLimelight(IDragonSensor::SENSOR_USAGE::MAIN_LIMELIGHT );
-    /*if (m_limelight.get() != nullptr )
-    {
-        m_limelight.get()->SetLEDMode( DragonLimelight::LED_MODE::LED_OFF);
-    } */  
-}
 
-/// @brief This function is called every robot packet, no matter the  mode. This is used for items like diagnostics that run 
-///        during disabled, autonomous, teleoperated and test modes (states).  THis runs after the specific state periodic 
-///        methods and before the LiveWindow and SmartDashboard updating.
-/// @return void
-void Robot::RobotPeriodic() 
-{
+
 
 }
 
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want ran during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {}
 
-/// @brief This initializes the autonomous state
-/// @return void
-void Robot::AutonomousInit() 
-{
-    m_chassisStateMgr->SetState( ChassisStateMgr::CHASSIS_STATE::AUTON );
+/**
+ * This autonomous (along with the chooser code above) shows how to select
+ * between different autonomous modes using the dashboard. The sendable chooser
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+ * remove all of the chooser code and uncomment the GetString line to get the
+ * auto name from the text box below the Gyro.
+ *
+ * You can add additional auto modes by adding additional comparisons to the
+ * if-else structure below with additional strings. If using the SendableChooser
+ * make sure to add them to the chooser code above as well.
+ */
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  std::cout << "Auto selected: " << m_autoSelected << std::endl;
 
-    // run selected auton option
-    //m_cyclePrims->Init();
-
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
 }
 
-
-/// @brief Runs every 20 milliseconds when the autonomous state is active.
-/// @return void
-void Robot::AutonomousPeriodic() 
-{
-    //Real auton magic right here:
-    //m_cyclePrims->RunCurrentPrimitive();
-
-    m_chassisStateMgr->RunCurrentState();
+void Robot::AutonomousPeriodic() {
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
 }
 
+void Robot::TeleopInit() {}
 
-/// @brief This initializes the teleoperated state
-/// @return void
-void Robot::TeleopInit() 
-{
-    m_chassisStateMgr->SetState( ChassisStateMgr::CHASSIS_STATE::TELEOP );
-    m_chassisStateMgr->Init();
-    m_powerCells->SetCurrentState(BallManipulator::BALL_MANIPULATOR_STATE::OFF);
-    m_powerCells->RunCurrentState();
-    // m_control->RunCurrentState();
-    // m_climber->RunCurrentState();
-}
-
-
-/// @brief Runs every 20 milliseconds when the teleoperated state is active.
-/// @return void
 void Robot::TeleopPeriodic() 
 {
-    auto pigeon = PigeonFactory::GetFactory()->GetPigeon();
-    if(pigeon != nullptr)
-    {
-        frc::SmartDashboard::PutBoolean("Pigeon", true);
-    }
-    else
-    {
-        frc::SmartDashboard::PutBoolean("Pigeon", false);
-    }
-    frc::SmartDashboard::PutNumber("yaw", pigeon->GetYaw());
-    m_chassisStateMgr->RunCurrentState();
-    //m_turret->Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_controller->GetAxisValue(TeleopControl::FUNCTION_IDENTIFIER::TURRET_MANUAL_AXIS) * .5);
-    m_shooterHood->SetOutput(ControlModes::PERCENT_OUTPUT, .5* m_controller->GetAxisValue(TeleopControl::SHOOTER_HOOD_MANUAL_AXIS));
-    //m_intake->RunCurrentState();
-   //m_powerCells->RunCurrentState();
-    // m_control->RunCurrentState();
-    //double leftSpeed = m_controller->GetAxisValue(TeleopControl::FUNCTION_IDENTIFIER::TANK_DRIVE_LEFT_CONTROL);
-    //double rightSpeed = m_controller->GetAxisValue(TeleopControl::FUNCTION_IDENTIFIER::TANK_DRIVE_RIGHT_CONTROL);
-    //m_chassis.get()->SetOutput(ControlModes::PERCENT_OUTPUT, leftSpeed, rightSpeed);
-    // m_climber->RunCurrentState();
-    //m_rightMaster->Set(ControlMode::PercentOutput, rightSpeed);
-    //m_leftMaster->Set(ControlMode::PercentOutput, leftSpeed);
-    //frc::SmartDashboard::PutNumber("leftSpeed", leftSpeed);
-    //frc::SmartDashboard::PutNumber("rightSpeed", rightSpeed);
-    frc::SmartDashboard::PutNumber("Turret position", m_turret->GetCurrentPosition());
-    frc::SmartDashboard::PutNumber("Shooter Hood position", m_shooterHood->GetCurrentPosition());
-    frc::SmartDashboard::PutNumber("Impeller speed", m_impeller->GetCurrentSpeed());
-    frc::SmartDashboard::PutNumber("Shooter speed", m_shooter->GetCurrentSpeed());
-    //frc::SmartDashboard::PutNumber("Limelight tx", m_limelight.get()->GetTargetHorizontalOffset());
+  auto y = -m_driver->GetY( GenericHID::JoystickHand::kLeftHand );
+  auto x = m_driver->GetX( GenericHID::JoystickHand::kRightHand );
+
+  auto left = x+y;
+  auto right = y-x;
+  auto maxValue = max(abs( left ), abs( right ) );
+  if ( maxValue > 1.0 )
+  {
+      left /= maxValue;
+      right /= maxValue;
+  }
+
+  m_left1->Set( ControlMode::PercentOutput, left );
+  m_left2->Set( ControlMode::PercentOutput, left );
+  m_right1->Set( ControlMode::PercentOutput, right );
+  m_right2->Set( ControlMode::PercentOutput, right );
+
+  m_climberSol->Set( m_driver->GetAButton( ) );
+  m_cpmSol->Set( m_driver->GetBButton( ) );
+  if ( m_driver->GetYButton( ) )
+  {
+    m_cpm->Set(ControlMode::PercentOutput, 0.5);
+  }
+  else
+  {
+    m_cpm->Set( ControlMode::PercentOutput, 0.0 );
+  }
+  
+  if ( m_driver->GetXButton( ) )
+  {
+    m_winch->Set(ControlMode::PercentOutput, 1.0);
+  }
+  else
+  {
+    m_winch->Set( ControlMode::PercentOutput, 0.0 );
+  }
+
+
+  m_intakeSol->Set( m_pilot->GetAButton() );
+  if ( m_pilot->GetAButton() )
+  {
+    m_intake->Set( ControlMode::PercentOutput, 0.7 );
+    m_impeller->Set( ControlMode::PercentOutput, 0.5 );    
+    m_transfer->Set( ControlMode::PercentOutput, 0.3 );
+
+  }
+  else if ( m_pilot->GetBButton() )
+  {
+    m_intake->Set( ControlMode::PercentOutput, -0.5 );
+    m_impeller->Set( ControlMode::PercentOutput, -0.5 );    
+    m_transfer->Set( ControlMode::PercentOutput, -1.0 );
+    m_shooter1->Set( ControlMode::PercentOutput, 0.75 );
+    m_shooter2->Set( ControlMode::PercentOutput, 0.75 );
+  }
+  else
+  {
+    m_intake->Set( ControlMode::PercentOutput, 0.0 );
+    m_impeller->Set( ControlMode::PercentOutput, 0.0 );    
+    m_transfer->Set( ControlMode::PercentOutput, 0.0 );
+  }
+
+  if ( m_pilot->GetXButton() )
+  {
+    m_shooter1->Set( ControlMode::PercentOutput, m_shooterSpeed );
+    m_shooter2->Set( ControlMode::PercentOutput, m_shooterSpeed );
+  }
+  else 
+  {
+    m_shooter1->Set( ControlMode::PercentOutput, 0.0 );
+    m_shooter2->Set( ControlMode::PercentOutput, 0.0 );
+
+  }
+
+  if ( m_pilot->GetBumper( GenericHID::JoystickHand::kLeftHand ) )
+  {
+    m_shooterSpeed *= 0.9;
+  }
+  else if ( m_pilot->GetBumper( GenericHID::JoystickHand::kRightHand) )
+  {
+    m_shooterSpeed *= 1.1;
+  }
+  SmartDashboard::PutNumber( "Shooter Percent ", m_shooterSpeed );
+
+
+  auto turretSpeed = m_pilot->GetX( GenericHID::JoystickHand::kLeftHand ) * 0.75;
+  m_turret->Set( ControlMode::PercentOutput, turretSpeed );
+  
+  auto hoodSpeed = -m_pilot->GetY( GenericHID::JoystickHand::kLeftHand ) * 0.5;
+  m_shooterHood->Set( ControlMode::PercentOutput, hoodSpeed );
+
 }
 
-
-
-/// @brief This initializes the test state
-/// @return void
-void Robot::TestInit() 
-{
-    m_testSelected = m_testChooser.GetSelected();
-    if ( m_testSelected == m_buttonBoxTest )
-    {
-        m_currentTest = BUTTON_BOX;
-        m_buttonBoxDisplay = new ButtonBoxDisplay();
-    }
-    else if ( m_testSelected == m_dragonXBoxTest )
-    {
-        m_currentTest = XBOX;
-        m_xBoxDisplay = new XboxDisplay();
-    }
-	else if ( m_testSelected == m_intakeTest )
-	{
-        m_currentTest = INTAKE;
-		m_intakeStateMgrTest = new IntakeStateMgrTest();
-	}
-	else if ( m_testSelected == m_impellerTest )
-	{
-        m_currentTest = IMPELLER;
-		m_impellerStateMgrTest = new ImpellerStateMgrTest();
-	}
-	else if ( m_testSelected == m_ballTransferTest )
-	{
-        m_currentTest = TRANSFER;
-		m_ballTransferStateMgrTest = new BallTransferStateMgrTest();
-	}
-    /**
-	else if ( m_testSelected == m_shooterTest )
-	{
-        m_currentTest = SHOOTER;
-		m_shooterStateMgrTest = new ShooterStateMgrTest();
-	}
-    **/
-    else
-    {
-        m_currentTest = NONE;
-    }
-}
-
-
-/// @brief Runs every 20 milliseconds when the test state is active.
-/// @return void
-void Robot::TestPeriodic() 
-{
-    switch ( m_currentTest )
-    {
-        case BUTTON_BOX:
-            m_buttonBoxDisplay->periodic();
-            break;
-
-        case XBOX:
-            m_xBoxDisplay->periodic();
-            break;
-			
-		case INTAKE: 
-			if ( !m_intakeStateMgrTest->IsDone() )
-			{
-				m_intakeStateMgrTest->Periodic();
-			}
-			break;
-			
-		case IMPELLER: 
-			if ( !m_impellerStateMgrTest->IsDone() )
-			{
-				m_impellerStateMgrTest->Periodic();
-			}
-			break;
-			
-		case TRANSFER: 
-			if ( !m_ballTransferStateMgrTest->IsDone() )
-			{
-				m_ballTransferStateMgrTest->Periodic();
-			}
-			break;
-		/**
-		case SHOOTER: 
-			if ( !m_shooterStateMgrTest->IsDone() )
-			{
-				m_shooterStateMgrTest->Periodic();
-			}
-			break;
-        **/
-        default:
-            break;
-    }
-}
+void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
-int main() 
-{
-    return StartRobot<Robot>(); 
-}
+int main() { return frc::StartRobot<Robot>(); }
 #endif
--- src/main/cpp/Robot.h
+++ src/main/cpp/Robot.h
@@ -1,131 +1,62 @@
-
-//====================================================================================================================================================
-// Copyright 2020 Lake Orion Robotics FIRST Team 302
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-// OR OTHER DEALINGS IN THE SOFTWARE.
-//====================================================================================================================================================
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 #pragma once
 
-//========================================================================================================
-/// Robot.h
-//========================================================================================================
-///
-/// File Description:
-///     Top-level robot code that controls the various states of the robot.  It is our specific 
-///     implementation of the frc::TimedRobot.
-///
-//========================================================================================================
-
-#pragma once
-
-// c++ includes
 #include <string>
-// wpilib includes
+
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonFX.h>
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonSRX.h>
+#include <frc/Solenoid.h>
+#include <frc/XboxController.h>
 
-// team 302 includes
-#include <auton/CyclePrimitives.h>
-#include <states/chassis/ChassisStateMgr.h>
-#include <states/BallManipulator.h>
-//#include <states/controlPanel/ControlPanelStateMgr.h>
-//#include <states/climber/ClimberStateMgr.h>
-#include <states/intake/IntakeStateMgr.h>
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
 
-#include <hw/DragonLimelight.h>
-#include <test/ButtonBoxDisplay.h>
-#include <test/XboxDisplay.h>
-#include <test/IntakeStateMgrTest.h>
-#include <test/ImpellerStateMgrTest.h>
-#include <test/BallTransferStateMgrTest.h>
-#include <test/ShooterStateMgrTest.h>
-#include <ctre/Phoenix.h>
-#include <gamepad/TeleopControl.h>
-#include <subsys/IMechanism.h>
-#include <hw/factories/LimelightFactory.h>
-#include <subsys/ChassisFactory.h>
-#include <subsys/IChassis.h>
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
 
-// third party includes
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX* m_left1;
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX* m_left2;
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX* m_right1;
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX* m_right2;
 
-/// @class Robot
-/// @brief Top-level robot code that controls the various states of the robot.  It is our specific 
-///        implementation of the frc::TimedRobot.
-class Robot : public frc::TimedRobot 
-{
-  public:
-      Robot() = default;
-      ~Robot() = default;
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX* m_shooter1;
+  ctre::phoenix::motorcontrol::can::WPI_TalonFX* m_shooter2;
+  
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* m_intake;
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* m_impeller;
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* m_transfer;  
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* m_turret;  
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* m_shooterHood;
 
-      void RobotInit() override;
-      void RobotPeriodic() override;
-      void AutonomousInit() override;
-      void AutonomousPeriodic() override;
-      void TeleopInit() override;
-      void TeleopPeriodic() override;
-      void TestInit() override;
-      void TestPeriodic() override;
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* m_cpm;
+  ctre::phoenix::motorcontrol::can::WPI_TalonSRX* m_winch;
 
-  private:
-      CyclePrimitives* m_cyclePrims;
-      ChassisStateMgr* m_chassisStateMgr;
-      //IntakeStateMgr* m_intake;
-      BallManipulator* m_powerCells;
-      // ControlPanelStateMgr* m_control;
-      // ClimberStateMgr* m_climer;
+  frc::Solenoid* m_intakeSol;
+  frc::Solenoid* m_cpmSol;
+  frc::Solenoid* m_climberSol;
 
+  frc::XboxController* m_driver;
+  frc::XboxController* m_pilot;
 
-      ButtonBoxDisplay* m_buttonBoxDisplay;
-      XboxDisplay* m_xBoxDisplay;
-	  IntakeStateMgrTest* m_intakeStateMgrTest;
-	  ImpellerStateMgrTest* m_impellerStateMgrTest;
-	  BallTransferStateMgrTest* m_ballTransferStateMgrTest;
-	  ShooterStateMgrTest* m_shooterStateMgrTest;
-
-      //std::shared_ptr<IChassis> m_chassis;
-      IMechanism* m_shooterHood;
-      IMechanism* m_turret;
-      TeleopControl* m_controller;
-      IMechanism* m_impeller;
-      IMechanism* m_shooter;
-      std::shared_ptr<DragonLimelight> m_limelight;
-      TalonFX* m_leftMaster;
-      TalonFX* m_leftSlave;
-      TalonFX* m_rightMaster;
-      TalonFX* m_rightSlave;
-
-
-      frc::SendableChooser<std::string>   m_testChooser;                    
-      const std::string                   m_noTest = "No Test";      
-      const std::string                   m_buttonBoxTest = "Button Box";      
-      const std::string                   m_dragonXBoxTest = "Dragon XBox";      
-	  const std::string                   m_intakeTest = "Intake";
-	  const std::string                   m_impellerTest = "Impeller";
-	  const std::string                   m_ballTransferTest = "Ball Transfer";
-//	  const std::string                   m_shooterTest = "Shooter";
-      std::string                         m_testSelected; 
-
-      enum TEST_TO_RUN
-      {
-          NONE,
-          BUTTON_BOX,
-          XBOX,
-		  INTAKE,
-		  IMPELLER,
-		  TRANSFER,
-		  SHOOTER
-      };
-      TEST_TO_RUN   m_currentTest;      
+  double m_shooterSpeed = 0.75;
 
 
 };
